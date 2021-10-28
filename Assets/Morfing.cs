83176b77334a9fa6880f4741326ed18c500e70c3
Pawel <mrskatervv@gmail.com>
2021-10-28 11:15:09 +0200

    Morfing Final

--- Assets/Morfing.cs
+++ Assets/Morfing.cs
@@ -5,72 +5,66 @@ using UnityEngine;
 public class Morfing : MonoBehaviour
 {
     [SerializeField]    GameObject Player;
-    [SerializeField]    GameObject[] BOX;
+    [SerializeField]    GameObject GroupObj;
     [SerializeField]    float MaxDistance=2.5f;
     [SerializeField]    float BackJump=3;
-   
-    MeshFilter PlayerMesh;
     Mesh OriginalMesh;
     Material OriginalMaterial;
-    RaycastHit hit;
-
-    private string CubeString="Cube";
+    RaycastHit Hit;
 
     void Start()
     {
-        PlayerMesh=Player.GetComponent<MeshFilter>();
-        OriginalMesh=PlayerMesh.sharedMesh;
+        OriginalMesh=Player.GetComponent<MeshFilter>().sharedMesh;
         OriginalMaterial=Player.GetComponent<Renderer>().material;    
     }
 
     void Update()
     {
-        
         int layerMask = 1 << 8;
-
         layerMask = ~layerMask;
-
         // Does the ray intersect any objects excluding the player layer
         if(Input.GetKeyDown(KeyCode.Mouse0))
         {
-            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, MaxDistance, layerMask))
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out Hit, MaxDistance, layerMask))
             {
-                Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
-                Debug.Log(hit.transform.name);
-                for(int i=0;i<BOX.Length;i++)
+                Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * Hit.distance, Color.yellow);
+                Debug.Log(Hit.transform.name);
+                for(int i=0;i<GroupObj.transform.childCount;i++)
                 {
-                    if(hit.transform.gameObject == BOX[i])
+                    if(Hit.transform.gameObject == GroupObj.transform.GetChild(i).gameObject)
                     {
-                        PlayerMesh.sharedMesh = Resources.Load<MeshFilter>(CubeString).sharedMesh;
+                        Player.GetComponent<MeshFilter>().sharedMesh = GroupObj.transform.GetChild(i).GetComponent<MeshFilter>().sharedMesh;
 
-                        if(hit.transform.GetComponent<CapsuleCollider>())
+                        if(Hit.transform.GetComponent<CapsuleCollider>())
                         {
-                            Player.GetComponent<CapsuleCollider>().height = hit.transform.GetComponent<CapsuleCollider>().height;
+                            Player.GetComponent<CapsuleCollider>().height = Hit.transform.GetComponent<CapsuleCollider>().height;
+                        }
+                        else if(Hit.transform.GetComponent<BoxCollider>())
+                        {
+                            Player.GetComponent<CapsuleCollider>().height = Hit.transform.GetComponent<BoxCollider>().size.y;
+                        }
+                        else
+                        {
+                            Debug.Log("None BoxCollider |  None CapsuleCollider");
                         }
                             
-                        else if(hit.transform.GetComponent<BoxCollider>())
-                        {
-                            Player.GetComponent<CapsuleCollider>().height = hit.transform.GetComponent<BoxCollider>().size.y;
-                        }
                             
-                        Player.GetComponent<Renderer>().material = hit.transform.GetComponent<Renderer>().material;
+                        Player.GetComponent<Renderer>().material = Hit.transform.GetComponent<Renderer>().material;
                     }
                 }
             }
             else
             {
-            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.white);
-            //Debug.Log("No Hit");
+                Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * MaxDistance, Color.white);
+                Debug.Log("No Hit");
             }
         }
-        if(Input.GetKeyDown(KeyCode.Mouse1))
+        else if(Input.GetKeyDown(KeyCode.Mouse1))
         {
-            Player.GetComponent<CapsuleCollider>().height=2;
             Player.GetComponent<Rigidbody>().velocity=new Vector3(0,BackJump,0);
-            PlayerMesh.sharedMesh=OriginalMesh;
+            Player.GetComponent<CapsuleCollider>().height=2;
+            Player.GetComponent<MeshFilter>().sharedMesh=OriginalMesh;
             Player.GetComponent<Renderer>().material=OriginalMaterial;
         }
-
-
     }
 }
