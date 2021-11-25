52ab5d1c298d0dfd45e8e210f45170719cf634d7
Dawid Kuczyński <dave04711@gmail.com>
2021-11-25 08:47:16 +0100

    rozne_pierdy #1

---
+++ Assets/Door.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    [SerializeField] LayerMask acceptableLayers;
+    Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        animator.SetBool("open", true);
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        animator.SetBool("open", false);
+    }
+}
