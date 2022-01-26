bdebfe5e4da6d7a780d56711a14e2d2d5ab2f7d2
Pawel <mrskatervv@gmail.com>
2022-01-26 14:21:52 +0100

    Door Perfab + All Audio

--- Assets/Door.cs
+++ Assets/Door.cs
@@ -5,16 +5,19 @@ using UnityEngine;
 public class Door : MonoBehaviour
 {   
     [SerializeField] LayerMask acceptableLayers;
+    [Space]
+    [SerializeField] AudioSource OpenSound;
+    [SerializeField] AudioSource CloseSound;
     Animator animator;
-    AudioSource OpenSound;
+    
     private void Awake()
     {
-        OpenSound=GetComponent<AudioSource>();
         animator = GetComponent<Animator>();
     }
 
     private void OnTriggerEnter(Collider other)
     {
+        CloseSound.Stop();
         animator.SetBool("open", true);
         OpenSound.Play();
     }
@@ -22,5 +25,6 @@ public class Door : MonoBehaviour
     {
         OpenSound.Stop();
         animator.SetBool("open", false);
+        CloseSound.Play();
     }
 }
