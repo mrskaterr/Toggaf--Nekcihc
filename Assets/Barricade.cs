aefc044dd35dce82a085afeab07f1d78d15b665c
Pawel <mrskatervv@gmail.com>
2022-02-03 10:52:15 +0100

    Dźwięk deprsji

---
+++ Assets/Barricade.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Barricade : MonoBehaviour
+{
+    AudioSource BarricadeSound;
+    void Awake(){
+        BarricadeSound=GetComponent<AudioSource>();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+
+        BarricadeSound.Play();
+        //other.gameObject.GetComponent<Rigidbody>().AddExplosionForce(1000f,transform.position,5f);
+
+    }
+}
